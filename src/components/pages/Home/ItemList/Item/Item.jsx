cce9d0e838aaeaaab84be22ae8137504baff5174
wdfwwkkee <denis.usmanov101@mail.ru>
2024-04-17 17:57:47 +0500

    addSnackBarForAdd to Cart

--- src/components/pages/Home/ItemList/Item/Item.jsx
+++ src/components/pages/Home/ItemList/Item/Item.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { actions } from '../../../../../Slices/cart.slice';
 import Price from '../../../../UI/Price';
 import { Link } from 'react-router-dom';
 
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
 
 const Item = ({ item }) => {
+
+    const [snackPack, setSnackPack] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [messageInfo, setMessageInfo] = useState(undefined);
+    useEffect(() => {
+        if (snackPack.length && !messageInfo) {
+            setMessageInfo({ ...snackPack[0] });
+            setSnackPack((prev) => prev.slice(1));
+            setOpen(true);
+        } else if (snackPack.length && messageInfo && open) {
+            setOpen(false);
+        }
+    }, [snackPack, messageInfo, open]);
+
+    const handleClick = (message, item) => () => {
+        dispatch(actions.addToCart(item))
+        setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+
+            setOpen(false);
+        }
+        setOpen(false);
+
+    };
+
+    const handleExited = () => {
+        setMessageInfo(undefined);
+    };
 
     const dispatch = useDispatch();
     const { cart } = useSelector(state => state)
@@ -45,8 +80,29 @@ const Item = ({ item }) => {
                     <button style={{ margin: 0 }} onClick={() => toggleDecrement(item)} >-
                     </button>
                 </div>
-                : <button onClick={() => dispatch(actions.addToCart(item))}>Add to Cart</button>
+                : <button onClick={handleClick(`The ${item.name} add to Cart`, item)}>Add to Cart</button>
             }
+            <Snackbar
+                key={messageInfo ? messageInfo.key : undefined}
+                open={open}
+                autoHideDuration={1500}
+                sx={{width : 550, "div" :{fontSize : "30px!important"}}}
+                onClose={handleClose}
+                TransitionProps={{ onExited: handleExited }}
+                message={messageInfo ? messageInfo.message : undefined}
+                action={
+                    <React.Fragment>
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            sx={{p : 0}}
+                            onClick={handleClose}
+                        >
+                            <CloseIcon sx={{ width : 30, "button" : {margin : 0}}} />
+                        </IconButton> 
+                    </React.Fragment>
+                }
+            />
         </div>
     )
 }
