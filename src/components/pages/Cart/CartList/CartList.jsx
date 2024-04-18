0c1d538268efac308759dbc07742ee6ebda9f5cb
wdfwwkkee <denis.usmanov101@mail.ru>
2024-04-18 13:45:23 +0500

    alertUserInComponentIfCartOrFavoriteListIsEmpty

--- src/components/pages/Cart/CartList/CartList.jsx
+++ src/components/pages/Cart/CartList/CartList.jsx
@@ -3,11 +3,12 @@ import CartItem from './CartItem/CartItem'
 import { useDispatch, useSelector } from 'react-redux'
 import Price from '../../../UI/Price'
 import { actions } from '../../../../Slices/cart.slice'
+import { Link } from 'react-router-dom'
 
 const CartList = () => {
 
   const { cart } = useSelector(state => state)
-  
+
   const dispatch = useDispatch();
 
   let sum = 0;
@@ -23,11 +24,15 @@ const CartList = () => {
   return (
     <div className='CartList'>
       <div>
-        {cart.map((itemCart, index) => <CartItem index={index} key={itemCart.id} item={itemCart} />)}
+        {cart.length !== 0 ?
+          cart.map((itemCart, index) => <CartItem index={index} key={itemCart.id} item={itemCart} />)
+          :
+          <div style={{ color: 'white', display: 'flex', flexDirection: 'column', textAlign: 'center', gap: 10 }}>Ваша корзина пуста <Link style={{ textDecoration: 'underline' }} to={'/fake-store/'}>Перейти к покупкам</Link> </div>
+        }
       </div>
       <div className="purchaseForm">
-        <div className='total' style={{display : 'flex', gap : 10}}>Total : <Price price={sum} /></div>
-      <button onClick={purchase}>Purchase</button>
+        <div className='total' style={{ display: 'flex', gap: 10 }}>Total : <Price price={sum} /></div>
+        <button onClick={purchase}>Purchase</button>
       </div>
     </div>
   )
